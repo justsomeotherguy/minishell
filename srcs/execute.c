d016a40d83bb193319900cc2058966c4a26a86d1
Jason Williamson <jwilliam@42adel.org.au>
2022-10-31 21:44:11 +1030

    more attempts at redirection

--- srcs/execute.c
+++ srcs/execute.c
@@ -6,7 +6,7 @@
 /*   By: jwilliam <jwilliam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/13 13:36:29 by jwilliam          #+#    #+#             */
-/*   Updated: 2022/10/26 15:44:03 by jwilliam         ###   ########.fr       */
+/*   Updated: 2022/10/31 20:27:42 by jwilliam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,21 +18,63 @@ static int	set_filein(t_cmdset *current, char *filein)
 {
 	int		f_in;
 	int		i;
+	int		j;
 	char	**trim;
 
 	f_in = open(filein, O_RDONLY);
+	i = 0;
+	j = 0;
 	while (current->tokens)
+		j++;
+	trim = (char **)malloc(sizeof(char *) * (j - 1));
+	j = 0;
+	while (current->tokens[j] && trim[i])
+	{
+		if (ft_strcmp(current->tokens[j], "<") == 0)
+			j += 2;
+		if (current->tokens[j])
+			trim[i] = ft_strdup(current->tokens[j]);
+		else
+			break ;
 		i++;
-	trim = (char **)malloc(sizeof(char *) * (i - 1));
-
+		j++;
+	}
+	trim[i] = 0;
+	free_2d_array(current->tokens);
+	current->tokens = trim;
 	return (f_in);
 }
 
 static int	set_fileout(t_cmdset *current, char *fileout)
 {
 	int		f_out;
+	int		i;
+	int		j;
+	char	**trim;
 
-	f_out = open(fileout, O_RDWR | O_CREAT | O_TRUNC, 0644);
+	f_out = open(fileout, O_WRONLY | O_CREAT | O_TRUNC, 0777);
+	i = 0;
+	j = 0;
+	while (current->tokens[j])
+		j++;
+	trim = (char **)malloc(sizeof(char *) * (j - 1));
+	j = 0;
+	while (current->tokens[j])
+	{
+		printf("filout token %s\n", current->tokens[j]);
+		if (ft_strcmp(current->tokens[j], ">") == 0)
+			j += 2;
+		if (current->tokens[j])
+			trim[i] = ft_strdup(current->tokens[j]);
+		else
+			break ;
+		printf("trim filout token %s\n", trim[i]);
+		i++;
+		j++;
+	}
+	trim[i] = 0;
+	free_2d_array(current->tokens);
+	current->tokens = trim;
 	return (f_out);
 }
 
@@ -40,26 +82,11 @@ static void	do_process(char **paths, t_cmdset *current, int *fd)
 {
 	char	*exec_path;
 
-	if (current->fd_in != STDIN_FILENO)
-	{
-		if (dup2(current->fd_in, STDIN_FILENO) == -1)
-		{
-			printf("in dup error\n");
-			return ;
-		}
-		close(current->fd_in);
-	}
-	if (current->fd_out != STDOUT_FILENO)
-	{
-		if (dup2(current->fd_out, STDOUT_FILENO) == -1)
-		{
-			printf("out dup error\n");
-			return ;
-		}
-		close(current->fd_out);
-	}
 	if (is_builtin(current->tokens) >= 0)
+	{
 		do_builtin(is_builtin(current->tokens), current->tokens);
+		return ;
+	}
 	if (!paths)
 	{
 		printf("Unable to build PATH\n");
@@ -71,12 +98,17 @@ static void	do_process(char **paths, t_cmdset *current, int *fd)
 		printf("%s: command not found\n", current->tokens[0]);
 		exit(1);
 	}
-	if (execve(exec_path, current->tokens, NULL) == -1)
+	current->pid = fork();
+	if (current->pid == 0)
 	{
-		printf("Unable to execute command\n");
-		exit(1);
+		if (execve(exec_path, current->tokens, NULL) == -1)
+		{
+			printf("Unable to execute command\n");
+			exit(1);
+		}
 	}
-	exit(0);
+	waitpid(current->pid, NULL, 0);
+	return ;
 }
 
 static t_cmdset	*has_redirect(void)
@@ -101,31 +133,29 @@ void	executor(void)
 			if (ft_strcmp(current->tokens[j], "<") == 0)
 			{
 				if (current->tokens[j + 1])
-					set_filein(current, current->tokens[j + 1]);
+				{	
+					current->fd_in = set_filein(current, current->tokens[j + 1]);
+					dup2(fd[0], current->fd_in);
+					close(fd[0]);
+					j = -1;
+				}
 			}
 			else if (ft_strcmp(current->tokens[j], ">") == 0)
 			{
 				if (current->tokens[j + 1])
-					set_fileout(current, current->tokens[j + 1]);
+				{	
+					current->fd_out = set_fileout(current, current->tokens[j + 1]);
+					dup2(fd[1], current->fd_out);
+					close(fd[1]);
+					j = -1;
+				}
 			}
 			j++;
 		}
-		fd[0] = current->fd_in;
-		fd[1] = current->fd_out;
 		if (pipe(fd) == -1)
 			printf("Pipe Error");
-		current->pid = fork();
-		if (current->pid < 0)
-		{
-			close(fd[0]);
-			close(fd[1]);
-			printf("Fork Error");
-			return ;
-		}
-		if (current->pid == 0)
-			do_process(paths, current, fd);
-		if (waitpid(current->pid, NULL, 0) < 0)
-			printf("Process error\n");
+		do_process(paths, current, fd);
 		current = current->next;
+//		close(fd[1]);
 	}
 }
