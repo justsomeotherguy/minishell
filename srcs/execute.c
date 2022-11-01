be4ff52312136682aa4feb8ad76dd87d61eac298
Jason Williamson <jwilliam@42adel.org.au>
2022-11-02 00:23:58 +1030

    still not redirecting

--- srcs/execute.c
+++ srcs/execute.c
@@ -6,7 +6,7 @@
 /*   By: jwilliam <jwilliam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/13 13:36:29 by jwilliam          #+#    #+#             */
-/*   Updated: 2022/10/31 20:27:42 by jwilliam         ###   ########.fr       */
+/*   Updated: 2022/11/02 00:23:15 by jwilliam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,9 +78,32 @@ static int	set_fileout(t_cmdset *current, char *fileout)
 	return (f_out);
 }
 
+static void	pipe_proc(t_cmdset *current, int *fd)
+{
+	char	**paths;
+	char	*exec_path;
+
+	paths = init_pathlist();
+	exec_path = get_path_for_cmd(paths, current->tokens[0]);
+	close(fd[0]);
+	close(fd[1]);
+	if (current->fd_in != 0)
+		close(current->fd_in);
+	if (current->fd_out != 1)
+		close(current->fd_out);
+	if (execve(exec_path, current->tokens, NULL) == -1)
+	{
+		printf("Unable to execute command\n");
+		return ;
+	}
+	return ;
+}
+
+/*
 static void	do_process(char **paths, t_cmdset *current, int *fd)
 {
 	char	*exec_path;
+
 
 	if (is_builtin(current->tokens) >= 0)
 	{
@@ -101,21 +124,90 @@ static void	do_process(char **paths, t_cmdset *current, int *fd)
 	current->pid = fork();
 	if (current->pid == 0)
 	{
+		close(fd[1]);
+		close(current->fd_in);
+		close(current->fd_out);
 		if (execve(exec_path, current->tokens, NULL) == -1)
 		{
 			printf("Unable to execute command\n");
-			exit(1);
+			return ;
 		}
 	}
 	waitpid(current->pid, NULL, 0);
+	close(fd[0]);
 	return ;
 }
+*/
 
-static t_cmdset	*has_redirect(void)
+static int	set_fd_in(t_cmdset *current)
 {
+	int		j;
 
+	j = 0;
+	while (current->tokens[j])
+	{
+		if (ft_strcmp(current->tokens[j], "<") == 0)
+		{
+			if (current->tokens[j + 1])
+			{	
+				current->fd_in = set_filein(current, current->tokens[j + 1]);
+				return (1);
+			}
+		}
+		j++;
+	}
+	return (0);
 }
 
+static int	set_fd_out(t_cmdset *current)
+{
+	int		j;
+
+	j = 0;
+	while (current->tokens[j])
+	{	
+		if (ft_strcmp(current->tokens[j], ">") == 0)
+		{
+			if (current->tokens[j + 1])
+			{	
+				current->fd_out = set_fileout(current, current->tokens[j + 1]);
+				return (1);
+			}
+		}
+		j++;
+	}
+	return (0);
+}
+
+void	executor(void)
+{
+	t_cmdset	*current;
+	int			fd[2];
+
+	current = g_super.cmds;
+	while (current)
+	{
+		if (set_fd_in(current) == 1)
+		{
+			if (dup2(current->fd_in, STDIN_FILENO) == -1)
+				return ;
+			close(current->fd_in);
+		}
+		set_fd_out(current);
+		if (pipe(fd) == -1)
+			return ;
+		current->pid = fork();
+		if (current->pid == -1)
+			return ;
+		if (current->pid == 0)
+			pipe_proc(current, fd);
+		close(fd[1]);
+		waitpid(current->pid, NULL, 0);
+		close(fd[0]);
+		current = current->next;
+	}
+}
+/*
 void	executor(void)
 {
 	char		**paths;
@@ -127,31 +219,7 @@ void	executor(void)
 	paths = init_pathlist();
 	while (current)
 	{
-		j = 0;
-		while (current->tokens[j])
-		{
-			if (ft_strcmp(current->tokens[j], "<") == 0)
-			{
-				if (current->tokens[j + 1])
-				{	
-					current->fd_in = set_filein(current, current->tokens[j + 1]);
-					dup2(fd[0], current->fd_in);
-					close(fd[0]);
-					j = -1;
-				}
-			}
-			else if (ft_strcmp(current->tokens[j], ">") == 0)
-			{
-				if (current->tokens[j + 1])
-				{	
-					current->fd_out = set_fileout(current, current->tokens[j + 1]);
-					dup2(fd[1], current->fd_out);
-					close(fd[1]);
-					j = -1;
-				}
-			}
-			j++;
-		}
+
 		if (pipe(fd) == -1)
 			printf("Pipe Error");
 		do_process(paths, current, fd);
@@ -159,3 +227,4 @@ void	executor(void)
 //		close(fd[1]);
 	}
 }
+*/
