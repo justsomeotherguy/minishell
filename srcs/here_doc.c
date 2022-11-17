9786a2eda5fb334be288dc69640484808254e6ad
Jason Williamson <jwilliam@42adel.org.au>
2022-11-17 18:40:34 +1030

    quick ammend to heredoc

--- srcs/here_doc.c
+++ srcs/here_doc.c
@@ -6,7 +6,7 @@
 /*   By: jwilliam <jwilliam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 13:28:54 by jwilliam          #+#    #+#             */
-/*   Updated: 2022/11/14 14:18:49 by jwilliam         ###   ########.fr       */
+/*   Updated: 2022/11/17 13:53:24 by jwilliam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int	get_heredoc(char *end)
 
 	fd = open("/tmp/.heredoc", O_CREAT | O_RDWR | O_TRUNC, 0644);
 	if (fd == -1)
-		return (1);
+		return (-1);
 	while (1)
 	{
 		line = readline("heredoc> ");
@@ -30,6 +30,7 @@ int	get_heredoc(char *end)
 		if (ft_strcmp(line, end) == 0)
 			break ;
 		write(fd, line, ft_strlen(line));
+		write(fd, "\n", 1);
 	}
 	return (fd);
 }
